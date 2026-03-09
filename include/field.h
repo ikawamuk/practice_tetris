1abac545bee115b1fa70c125ff510f09355ee67a
ikawamuk <ikawamuk@student.42.fr>
2026-03-10 00:08:50 +0900

    next is use hold

--- include/field.h
+++ include/field.h
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 03:29:32 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/03/08 03:30:42 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/03/10 00:08:33 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 
 # include "define.h"
 # include "mino.h"
+# include <stdbool.h>
 
 typedef struct
 {
 	int				terrain[FIELD_HEIGHT][FIELD_WIDTH];
 	t_target_mino	target;
+	t_mino			*hold;
+	bool			use_hold_this_turn;
 }	t_field;
 
 #endif
--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 02:30:03 by ikawamuk          #+#    #+#             */
-/*   Updated: 2026/03/09 23:48:34 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2026/03/10 00:03:10 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,32 @@
 
 void	draw_screen(t_field *field);
 
+static void	refill_pool(t_mino *pool, int *pool_size)
+{
+	t_mino	tmp;
+	int		j;
+
+	for (int i = 0; i < 7; i++)
+		pool[i] = minos[i];
+	for (int i = 6; i > 0; i--)
+	{
+		j = rand() % (i + 1);
+		tmp = pool[i];
+		pool[i] = pool[j];
+		pool[j] = tmp;
+	}
+	*pool_size = 7;
+}
+
 void	init(t_field *field)
 {
-	field->target.mino = minos[rand() % 7];
+	static t_mino	pool[7];
+	static int		pool_size = 0;
+
+	if (pool_size == 0)
+		refill_pool(pool, &pool_size);
+	field->target.mino = pool[--pool_size];
 	field->target.x = (FIELD_WIDTH - 4) / 2;
 	field->target.y = 0;
 	draw_screen(field);
-	// fprintf(stderr, "mino: %d\n", field->target.mino.id);
-	return ;
 }
